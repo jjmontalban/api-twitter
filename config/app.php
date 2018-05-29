1e745967d84f66f1359c61695aabb98b9a2c12ea
JJMontalban <jjmontalban@gmail.com>
2018-05-29 12:51:22 +0200

    integración librería thujohn/twitter

--- config/app.php
+++ config/app.php
@@ -159,7 +159,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        'Thujohn\Twitter\TwitterServiceProvider',
+        Thujohn\Twitter\TwitterServiceProvider::class,
 
     ],
 
@@ -209,7 +209,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Twitter' => 'Thujohn\Twitter\TwitterFacade',
+        'Twitter' => 'Thujohn\Twitter\Facades\Twitter',
 
     ],
 
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/twitter', function()
+{
+	//vamos a obtener el JSON que genera la API. El contenido de este JSON lo vamos a almacenar en la variable $html:
+    $html = Twitter::getUserTimeline(['screen_name' => 'jjmontalban', 'count' => 20, 'format' => 'json']);
+
+    // Una vez que hemos obtenido el JSON crudo que nos envia la API decodificamos el JSON con la función json_decode
+    $json = json_decode($html);
+
+    // Una vez que tenemos almacenada toda la información del JSON obtenido de la API vamos a extraer la información útil de el, para 
+    // ello vamos a almacenar en variables con nombres orientativos la información del JSON
+    dd($json);
+});
+
